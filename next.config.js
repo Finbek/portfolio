a76377cd4da3a0300319c2ef909a42472def76d6
Dilshod Bakhtiyorov <46556037+Finbek@users.noreply.github.com>
2023-04-15 06:40:08 +0900

    trying akamai image loader

--- next.config.js
+++ next.config.js
@@ -1,29 +1,13 @@
 const path = require("path");
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: "https://finbek.imgix.net/",
-  },
   reactStrictMode: true,
-  loader: "imgix",
-  path: 'the "domain" of your Imigix source',
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `@import "media.scss";`,
