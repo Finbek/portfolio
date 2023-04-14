9bfb31a8bff7b4738110954804a54625a84aac85
Dilshod Bakhtiyorov <46556037+Finbek@users.noreply.github.com>
2023-04-15 04:25:13 +0900

    cicd: github actions

--- next.config.js
+++ next.config.js
@@ -1,13 +1,33 @@
 const path = require("path");
 
+const repo = "p";
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `@import "media.scss";`,
   },
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.svg$/,
